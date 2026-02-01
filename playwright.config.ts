040c602a89c016b2a6bc0aa689efa08ec8a5eaa3
Dani-Pomareta <daniel.pomareta.moratalla@gmail.com>
2026-02-01 20:39:28 +0900

    fix: isolation logic and screenshot #billing

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    screenshot: "only-on-failure",
+    screenshot: "only-on-failure", // This is the magic line
+    video: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */

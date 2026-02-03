114f70ca5972bab2b998a4eced16a8e5714c3a11
Dani-Pomareta <daniel.pomareta.moratalla@gmail.com>
2026-02-04 06:39:09 +0900

    fix: k6 syntax and consolidate to single quality bundle #perf

--- tests_perf/smoke-load.js
+++ tests_perf/smoke-load.js
@@ -3,10 +3,15 @@ import { check, sleep } from "k6";
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export function handleSummary(data) {
+  console.log('Preparing the breakdown...');
   return {
-    "k6-report.html": htmlReport(data),
+    'stdout': textSummary(data, { indent: ' ', enableColors: true }), // Show in logs
+    'k6-summary.json': JSON.stringify(data), // For data lovers
   };
 }
+
+// Helper to make the logs look nice
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
 
 export const options = {
   vus: 5, // 5 concurrent users
@@ -16,7 +21,7 @@ export const options = {
   },
 };
 
-export default function () {
+export default function smokeLoad () {
   const res = http.get("https://www.saucedemo.com/");
   check(res, { "status was 200": (r) => r.status === 200 });
   sleep(1);
